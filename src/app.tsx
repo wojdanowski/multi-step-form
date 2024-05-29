1496937d5b5890ec299db867a8ae77fbdefd5bb1
Sebastian Wojdanowski <sebastian.w@moodup.team>
2024-05-29 12:51:23 +0200

    feat(reset): reset this commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,41 +1,90 @@
-import { useState } from "react";
-import "./app.css";
+import './app.css';
 
 export function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div className="container">
+        <div className="content-container">
+          <div className="sidebar">
+            {/* ---------------------- */}
+            <div className="sidebar__row">
+              <div className="sidebar__circle-container">
+                <div className="sidebar__circle sidebar__circle--active">
+                  <p>1</p>
+                </div>
+              </div>
+              <div className="sidebar__description">
+                <p>STEP 1</p>
+                <h4>YOUR INFO</h4>
+              </div>
+            </div>
+            {/* ---------------------- */}
+            <div className="sidebar__row">
+              <div className="sidebar__circle-container">
+                <div className="sidebar__circle">
+                  <p>1</p>
+                </div>
+              </div>
+              <div className="sidebar__description">
+                <p>STEP 1</p>
+                <h4>YOUR INFO</h4>
+              </div>
+            </div>
+            {/* ---------------------- */}
+          </div>
+
+          <div className="selection-config-box__container">
+            <div className="selection-config-box">
+              <div id="step-content">
+                <div className="personal-info__header">
+                  <h1>Personal Info</h1>
+                  <p>
+                    Please provide your name, email address, and phone number.
+                  </p>
+                  <div className="form-container">
+                    <form action="">
+                      <div className="form-row">
+                        <div className="label-container">
+                          <label htmlFor="fname">Name</label>
+                        </div>
+                        <input type="text" id="fname" name="fname" />
+                      </div>
+
+                      <div className="form-row">
+                        <div className="label-container">
+                          <label htmlFor="femail">Email address</label>
+                        </div>
+                        <input type="text" id="femail" name="femail" />
+                      </div>
+
+                      <div className="form-row">
+                        <div className="label-container">
+                          <label htmlFor="fphone">Phone Number</label>
+                          <label className="invalid" htmlFor="femail">
+                            This field is required
+                          </label>
+                        </div>
+                        <input
+                          className="invalid"
+                          type="text"
+                          id="fphone"
+                          name="fphone"
+                        />
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+              <div className="plan-box__nav">
+                <button className="button--secondary" type="button">
+                  Go Back
+                </button>
+                <button type="button">Next Step</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
